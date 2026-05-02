8e2272d0378c1eb3f45313bd2013bd6306989bc9
guillermouberomero <guillermo.uberomero@alumnos.upm.es>
2026-05-02 15:08:50 +0200

    prueba clase GUR

--- 02_React/Codigo de apoyo/front-end-supplies-00-orientacion-componentes/src/components/Footer/Footer.jsx
+++ 02_React/Codigo de apoyo/front-end-supplies-00-orientacion-componentes/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>© 2025 UNIR Supplies. Todos los derechos reservados.</p>
+            <p>© 2025 UNIR Supplies. Todos los derechos reservados. GUR</p>
         </footer>
     );
 }
